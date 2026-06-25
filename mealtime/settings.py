7804f61106df37e1747e663ae0a226da085a46bf
AbhiramK16 <kuchiabhiram@gmail.com>
2026-06-24 23:59:05 -0700

    Add .onrender.com to ALLOWED_HOSTS, create tracker migration

--- mealtime/settings.py
+++ mealtime/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY') or 'django-insecure-vercel-temporary-fallba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.onrender.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
